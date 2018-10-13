c38dccf7baa6e8f13d530436ba3f2eba40657ba4
Peijun Zhu <zpj.ustc@gmail.com>
2018-10-13 15:02:28 -0400

    adding comments

--- btree.h
+++ btree.h
@@ -6,176 +6,212 @@
 #include <algorithm>
 
 using namespace std;
-
-template <typename T, int m>
+/**
+ * @brief Node class of btree. It has keys, child nodes and also size of keys
+ * @details The degree of a node is actually size + 1.
+ *  + If degree > D, it is overflowed
+ *  + If 2 * degree < D, it is underflowed
+ * overflowed/underflowed nodes must be processed by their parents!
+ *
+ * @tparam T data type for keys
+ * @tparam D Maximum possible degree (i.e. branching factor)
+ */
+template <typename T, int D>
 struct btree_node {
-    static_assert(m > 2, "Order of btree_node should be higher than 2");
-    static_assert(m < 128, "Order of btree_node should be smaller than 128");
-    btree_node* children[m+1] = {nullptr};
-    T sep[m-1+1];
-    int8_t fill = 0;
+    static_assert(D > 2, "D should be higher than 2");
+    static_assert(D < 127, "D should be smaller than 127");
+
+    int8_t size = 0;    ///< Size of keys
+    T keys[D-1+1];      ///< Keys as separators
+    btree_node* children[D+1] = {nullptr};  ///< Pointers to child nodes
+
+    int8_t degree() const{
+        return size + 1;
+    }
     bool is_leaf() const {
         return !children[0];
     }
     bool full() const{
-        return fill == m-1;
+        return degree() == D;
     }
     bool empty() const{
-        return fill == 0;
+        return size == 0;
     }
     bool overflowed() const{
-        return fill >= m;
+        return degree() > D;
     }
     bool underflowed() const{
-        return fill < (m-1)/2;
+        return 2 * degree() < D;
     }
+    /**
+     * @brief superfluous Does the node underflow when its degree is reduced by 1
+     */
     bool superfluous() const{
-        return fill > (m-1)/2;
+        return 2 * (degree()-1) >= D;
     }
 
-    void insert_sep(T val, int pos){
-        for (int i = fill; i>pos; i--){
-            sep[i] = sep[i-1];
+    void insert_key(T val, int pos){
+        for (int i = size; i>pos; i--){
+            keys[i] = keys[i-1];
         }
-        sep[pos] = val;
-        fill++;
-    }
-
-    void erase_sep(int pos){
-        for (int i = pos+1; i<fill; i++){
-            sep[i-1] = sep[i];
-        }
-        fill--;
-    }
-    void erase_child(int pos){
-        // Must be called before erasion of separator
-        for (int i = pos+1; i<fill+1; i++){
-            children[i-1] = children[i];
-        }
+        keys[pos] = val;
+        size++;
     }
     void insert_child(btree_node *val, int pos){
-        //Must be called after insertion of separator
-        for (int i = fill; i>pos; i--){
+        //Must be called after insertion of a key
+        for (int i = size; i>pos; i--){
             children[i] = children[i-1];
         }
         children[pos] = val;
     }
 
+    void erase_child(int pos){
+        // Must be called before erasion of a key
+        for (int i = pos+1; i<size+1; i++){
+            children[i-1] = children[i];
+        }
+    }
+    void erase_key(int pos){
+        for (int i = pos+1; i<size; i++){
+            keys[i-1] = keys[i];
+        }
+        size--;
+    }
+
+    /**
+     * @brief absorb Absorb keys and children of rhs
+     * @param rhs   The node to be absorbed
+     * @param pivot The separator between current node and rhs
+     */
     void absorb(btree_node* rhs, T pivot){
-        sep[fill] = pivot;
-        copy(rhs->sep, rhs->sep+rhs->fill, sep+fill+1);
-        copy(rhs->children, rhs->children+rhs->fill+1, children+fill+1);
-        fill += rhs->fill + 1;
+        keys[size] = pivot;
+        copy(rhs->keys, rhs->keys+rhs->size, keys+size+1);
+        copy(rhs->children, rhs->children+rhs->size+1, children+size+1);
+        size += rhs->size + 1;
         delete rhs;
     }
 
     friend ostream&
     operator<< (ostream &os, const btree_node * n){
-        if (!n->fill){
+        if (!n->size){
             os << "()";
         }
         else{
             os << "(";
-            for (int i=0; i<n->fill; i++)
-                os<<n->sep[i]<<", ";
+            for (int i=0; i<n->size; i++)
+                os<<n->keys[i]<<", ";
             os << "\b\b)";
         }
         return os;
     }
 };
 
-template <typename T, int m>
-void recursive_stream (ostream& os, btree_node<T, m> *n){
+template <typename T, int D>
+void recursive_stream (ostream& os, btree_node<T, D> *n){
     if (!n->is_leaf()){
         os << n << ":\t";
-        for (int i=0; i < n->fill+1; i++){
+        for (int i=0; i < n->size+1; i++){
             os << n->children[i]<<"\t";
         }
         os << endl;
-        for (int i=0; i < n->fill+1; i++){
+        for (int i=0; i < n->size+1; i++){
             recursive_stream(os, n->children[i]);
         }
     }
 }
 
 
-template <typename T, int m>
+template <typename T, int D>
 class btree{
 public:
-    using node_t = btree_node<T, m>;
+    using node_t = btree_node<T, D>;
     int size() const{return _size;}
     int depth() const{return _depth;}
 
+    /**
+     * @brief spill_leaf Spill keys of overflowed leaf node
+     * @param n overflowed node
+     * @return a new node storing right half of the original node
+     */
     static node_t *
-    overflow_leaf(node_t *n){
+    spill_leaf(node_t *n){
         node_t * rhs = new node_t;
-        copy(n->sep+m-m/2, n->sep+m, rhs->sep);
-        n->fill = m - m/2 - 1;
-        rhs->fill = m/2;
+        copy(n->keys+D-D/2, n->keys+D, rhs->keys);
+        n->size = D - D/2 - 1;
+        rhs->size = D/2;
         return rhs;
     }
-
+    /**
+     * @brief spill_node Spill keys and children of overflowed non-leaf node
+     * @param n overflowed node
+     * @return new node storing right half of the original node
+     */
     static node_t *
-    overflow_node(node_t *n){
-        node_t * rhs = overflow_leaf(n);
-        copy(n->children+m-m/2, n->children+m+1, rhs->children);
+    spill_node(node_t *n){
+        node_t * rhs = spill_leaf(n);
+        copy(n->children+D-D/2, n->children+D+1, rhs->children);
         return rhs;
     }
+    /**
+     * @brief level_up The level of BTree goes up by one
+     * @details This is done after root node overflowed and spilled into rhs
+     * @param rhs   Second child of the new root
+     */
     void level_up(node_t *rhs){
-//        cout<<">>> Level up"<<endl;
-        T root_sep = root->sep[root->fill];
+        T root_sep = root->keys[root->size];
         node_t * old = root;
         root = new node_t;
         root->children[0] = old;
         root->children[1] = rhs;
-        root->sep[0] = root_sep;
-        root->fill = 1;
+        root->keys[0] = root_sep;
+        root->size = 1;
         _depth++;
     }
+    /**
+     * @brief level_down The level of BTree goes down by one
+     * @details This is done when root node is empty
+     */
     void level_down(){
-//        cout<<">>> Level down"<<endl;
         node_t * old = root;
         root = root->children[0];
         delete old;
         _depth--;
     }
+    /**
+     * @brief insert_from Insert value from current node
+     * @param n current node
+     * @param val
+     * @return If current node overflows, return pointer to spilled node
+     */
     node_t* insert_from(node_t *n, T val){
-        int pos = upper_bound(n->sep, n->sep+n->fill, val) - n->sep;
+        int pos = upper_bound(n->keys, n->keys+n->size, val) - n->keys;
         if (n->is_leaf()){
-//            cout<<"Insert "<<val<<" at "<<pos<<" of "<<n<<endl;
-//            cout<<"After insertion: "<<endl;
-//            n->print();
-            n->insert_sep(val, pos);
+            n->insert_key(val, pos);
             if (n->overflowed()){
-//                cout<<"overflow leaves at "<< n->sep[m-m/2-1]<<" for "<<n<<endl;
-                return overflow_leaf(n);
+                return spill_leaf(n);
             }
-//            return nullptr;
         }
         else{
-//            cout<<"Insert "<<val<<" at "<<pos<<" of "<<n<<endl;
             node_t * child = n->children[pos];
             node_t * child_new = insert_from(child, val);
             if (child_new){
-//                cout<<"Before child insertion"<<n<<endl;
-//                cout<<pos<<child->sep[child->fill]<<endl;
-                n->insert_sep(child->sep[child->fill], pos);
+                n->insert_key(child->keys[child->size], pos);
                 n->insert_child(child_new, pos+1);
-//                cout<<"After child insertion"<<n<<endl;
                 if (n->overflowed()){
-//                    cout<<"overflow node at "<< n->sep[m-m/2-1]<<" for "<<n<<endl;
-                    return overflow_node(n);
+                    return spill_node(n);
                 }
             }
-//            return nullptr;
         }
         return nullptr;
     }
+    /**
+     * @brief insert Insert value into BTree
+     * @param val
+     */
     void insert(T val){
         if (!root){
             root = new node_t;
-//             cout<<int(root->fill)<<endl;
-            root->insert_sep(val, 0);
+            root->insert_key(val, 0);
         }
         else{
             node_t * overflow = insert_from(root, val);
@@ -184,81 +220,89 @@ public:
             }
         }
     }
+    /**
+     * @brief underflow Underflow handling
+     * @param n current node
+     * @param pos The position of the children that underflowed
+     */
     void underflow(node_t *n, int pos){
         node_t *self = n->children[pos];
-//         cout<<"Underflow handling "<<n<<endl;
         if (pos>0 && n->children[pos-1]->superfluous()){
-//             cout<<"Left rotation "<<endl;
+            /// Rotate child from left sibling
             node_t * sibling = n->children[pos-1];
-            // Rotate from left sibling
-            self->insert_sep(n->sep[pos-1], 0);
-            self->insert_child(sibling->children[sibling->fill], 0);
-            n->sep[pos-1] = sibling->sep[sibling->fill-1];
-            sibling->erase_child(sibling->fill);
-            sibling->erase_sep(sibling->fill-1);
-//             sibling->fill--;
+            self->insert_key(n->keys[pos-1], 0);
+            self->insert_child(sibling->children[sibling->size], 0);
+            n->keys[pos-1] = sibling->keys[sibling->size-1];
+            sibling->erase_child(sibling->size);
+            sibling->erase_key(sibling->size-1);
         }
-        else if(pos < n->fill+1  && n->children[pos+1]->superfluous()){
-            // Rotate from right sibling
-//             cout<<"Right rotation at "<<pos<<endl;
+        else if(pos < n->size+1  && n->children[pos+1]->superfluous()){
+            /// Rotate child from right sibling
             node_t * sibling = n->children[pos+1];
-            self->insert_sep(n->sep[pos], self->fill);
-            self->insert_child(sibling->children[0], self->fill);
-            n->sep[pos] = sibling->sep[0];
+            self->insert_key(n->keys[pos], self->size);
+            self->insert_child(sibling->children[0], self->size);
+            n->keys[pos] = sibling->keys[0];
             sibling->erase_child(0);
-            sibling->erase_sep(0);
+            sibling->erase_key(0);
         }
         else{
-            // Merge with a sibling
-//             cout<<"Merging"<<endl;
+            /// Merge with a sibling
             bool left_merge = (pos > 0);
             int mid = pos-left_merge;
             node_t *lhs = n->children[pos-left_merge], *rhs = n->children[pos+1-left_merge];
-            lhs->absorb(rhs, n->sep[mid]);
+            lhs->absorb(rhs, n->keys[mid]);
             n->erase_child(mid+1);
-            n->erase_sep(mid);
+            n->erase_key(mid);
         }
     }
-    void sep_by_max(node_t *n, T *p){
-//         cout<<"Separate by max from "<<n<<endl;
-        if (n->is_leaf()){
-            *p = n->sep[n->fill-1];
-//             cout<<"New separator found: "<<*p<<endl;
-            n->fill--;
+    /**
+     * @brief set_max_key Set key by max key of current node
+     * @param n current node
+     * @param key_p Pointer to key that needs to be reset
+     */
+    void set_max_key(node_t *n, T *key_p){
+        if (n->is_leaf()){ ///< Separator found
+            *key_p = n->keys[n->size-1];
+            n->size--;
         }
-        else{
-            node_t *last_child = n->children[n->fill];
-            sep_by_max(last_child, p);
+        else{   ///< Not found, recursively search
+            node_t *last_child = n->children[n->size];
+            set_max_key(last_child, key_p);
             if (last_child->underflowed()){
-//                 cout<<"Last child underflowed for "<<n<<endl;
-                underflow(n, n->fill);
+                underflow(n, n->size);
             }
         }
     }
+    /**
+     * @brief erase_from Erase from a given node
+     * @param n current node
+     * @param val value
+     */
     void erase_from(node_t *n, T val){
-        T* p = lower_bound(n->sep, n->sep+n->fill, val);
-        int i = p - n->sep;
-//         cout<<"Erase "<<val<<" from "<<n<<" found "<<i<<endl;
-        if (i!=n->fill && *p == val){
-            if(n->is_leaf()){
-                n->erase_sep(i);
+        T* key_p = lower_bound(n->keys, n->keys+n->size, val);
+        int i = key_p - n->keys;
+        if (i!=n->size && *key_p == val){///< Found val at current node
+            if(n->is_leaf()){   ///< For leaf node, simply delete
+                n->erase_key(i);
             }
-            else{
-                sep_by_max(n->children[i], p);
+            else{   ///< For non-leaf, set new key to be max of left child
+                set_max_key(n->children[i], key_p);
                 if (n->children[i]->underflowed()){
-//                     cout<<"After erasion: child underflowed for "<<n<<" at "<<i<<endl;
                     underflow(n, i);
                 }
             }
         }
-        else if(!n->is_leaf()){
+        else if(!n->is_leaf()){ ///< Not found, search recursively
             erase_from(n->children[i], val);
             if (n->children[i]->underflowed()){
-//                 cout<<"After erasion: child underflowed for "<<n<<" at "<<i<<endl;
                 underflow(n, i);
             }
         }
     }
+    /**
+     * @brief erase Erase value from the btree
+     * @param val value
+     */
     void erase(T val){
         if (root){
             erase_from(root, val);
@@ -268,7 +312,7 @@ public:
         }
     }
     friend ostream& operator<<(ostream& os, btree& bt){
-        os <<"BTree: D="<<bt.depth()<< ", m="<<m<< endl;
+        os <<"BTree: D="<<bt.depth()<< ", m="<<D<< endl;
         if (bt.root){
             if (bt.root->is_leaf()){
                 os << bt.root << endl;
